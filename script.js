3aea39c22c5ea408827e5e057cc30127459dab5b
Vincent Hunter <jhearst29@gmail.com>
2023-12-20 22:52:50 -0500

    Second Commit;Finished the program

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("This is a javascript test");
+const messageObj = {
+    messageFirst: ['Today is', 'You will be', 'Everything is'],
+    messageSecond: ['A great day', 'An okay circumstance', 'average at best'],
+    messageThird: ["you will still be fine", "you will fall apart and come back stronger", "you will work hard and be greater"]
+}
+
+let affirmation = [];
+
+const randomNumber = (num) => {
+    //gets a random number
+    return Math.floor(Math.random() * num)
+}
+
+for (let quote in messageObj) {
+    let quoteIndex = randomNumber(messageObj[quote].length)
+
+    switch (quote) {
+        case 'messageFirst':
+            affirmation.push(`Your message is: ${messageObj[quote][quoteIndex]}`)
+            break;
+        case 'messageSecond':
+            affirmation.push(`Your message is: ${messageObj[quote][quoteIndex]}`)
+            break;
+        case 'messageThird':
+            affirmation.push(`Your message is: ${messageObj[quote][quoteIndex]}`)
+            break;
+        default:
+            affirmation.push("Be cautious the outome is unclear.")
+
+    }
+}
+
+const displayMessage = (affirm) => {
+    const dailyReminder = affirmation.join('\n');
+    console.log(dailyReminder);
+}
+
+displayMessage(affirmation)
